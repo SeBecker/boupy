623dbcc528fc48a9e6303ad628b110f73403db31
Sebastian Becker <s3sebeck@uni-bonn.de>
2019-02-08 22:36:45 +0100

    Deleted some unnecessary imports

--- boupy/read/read.py
+++ boupy/read/read.py
@@ -1,13 +1,12 @@
 """The module contains the function of the init file import process."""
 import yaml
 
-from grmpy.read.read_auxiliary import init_dict_to_attr_dict
-from grmpy.check.check import check_presence_init
-
 
 def read(file):
-    """This function processes the initialization file so that it can be used for simulation purposes."""
-    
+    """This function processes the initialization file so that it can
+    be used for simulation purposes.
+    """
+
     # Check if there is a init file with the specified filename
 
     # Load the initialization file
@@ -15,13 +14,12 @@ def read(file):
         init_dict = yaml.load(y)
 
     # Create auxiliary section
-    init_dict['AUX'] = {}
-    init_dict['AUX']['params'] = [
-        init_dict['PARAMS']['alpha'] +
-        init_dict['PARAMS']['theta'] +
-        init_dict['DIST']['mu'] +
-        init_dict['DIST']['beta']
+    init_dict["AUX"] = {}
+    init_dict["AUX"]["params"] = [
+        init_dict["PARAMS"]["alpha"]
+        + init_dict["PARAMS"]["theta"]
+        + init_dict["DIST"]["mu"]
+        + init_dict["DIST"]["beta"]
     ]
 
-    
     return init_dict
