968287ddc2b4ef59b9a855223b224c4a254a39ca
Sebastian Becker <s3sebeck@uni-bonn.de>
2019-02-08 20:44:17 +0100

    Added config file/version file and __init__.py

---
+++ boupy/__init__.py
@@ -0,0 +1,16 @@
+"""The module allows to run tests from inside the interpreter."""
+import os
+
+import pytest
+
+from boupy.simulate.simulate import simulate
+from boupy.boupy_config import PACKAGE_DIR
+import grmpy.grmpy_config
+
+
+def test():
+    """The function allows to run the tests from inside the interpreter."""
+    current_directory = os.getcwd()
+    os.chdir(PACKAGE_DIR)
+    pytest.main()
+    os.chdir(current_directory)
